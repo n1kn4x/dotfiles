e516fb1d6c42b78343e35e40e3c4f4fe3023a5fc
Niklas Pirnay <n.pirnay@tu-berlin.de>
2025-01-13 10:00:28 +0100

    added: rust plugins

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -79,14 +79,42 @@ local plugins = {
     end,
   },
 
-  -- Plugin for RUST development 
+  -- Plugins for RUST development 
   {
     'mrcjkb/rustaceanvim',
     version = '^5', -- Recommended
     lazy = false, -- This plugin is already lazy
   },
 
-  -- Plugin for file managin using Oil.nvim
+  {
+    'mfussenegger/nvim-dap',
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end
+  },
+
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = {'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio'},
+    config = function()
+      require('dapui').setup()
+    end
+  },
+
+
+  -- Plugin for file managing using Oil.nvim
   {
     'stevearc/oil.nvim',
     ---@module 'oil'
